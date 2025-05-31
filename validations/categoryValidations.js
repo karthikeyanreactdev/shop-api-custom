91bcc4c9aedc2b185288513ac112181109407278
deepakdk303 <kumard312@gmail.com>
2025-05-31 11:32:43 +0530

    Update categoryValidations.js

--- validations/categoryValidations.js
+++ validations/categoryValidations.js
@@ -4,7 +4,7 @@ const Joi = require('joi');
 const categorySchema = Joi.object({
   name: Joi.string().required().min(2).max(100),
   description: Joi.string().max(500),
-  parentId: Joi.string().allow(null),
+  parentId: Joi.string().allow(null, '').optional(),
   level: Joi.number().integer().min(0).max(5).default(0),
   isActive: Joi.boolean().default(true),
   isFeatured: Joi.boolean().default(false),
